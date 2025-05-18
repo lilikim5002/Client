295cf4cc14fecaa9e30f4b7b8c425811027725c1
lilikim5002 <130602635+lilikim5002@users.noreply.github.com>
2025-05-18 12:44:20 +0300

    commit

--- src/app/shop/product-details/product-details.component.ts
+++ src/app/shop/product-details/product-details.component.ts
@@ -76,7 +76,7 @@ export class ProductDetailsComponent implements OnInit {
       if (this.quantity > this.quantityInBasket) {
         const itemsToAdd = this.quantity - this.quantityInBasket;
         this.quantityInBasket += itemsToAdd;
-        // this.basketService.addItemToBasket(this.product, itemsToAdd);
+        this.basketService.addItemToBasket(this.product, itemsToAdd);
       } else {
         const itemsToRemove = this.quantityInBasket - this.quantity;
         this.quantityInBasket -= itemsToRemove;
